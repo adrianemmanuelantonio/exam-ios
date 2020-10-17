22c854e08d37dde221a3a38eb1043af18e8aab89
Adrian Antonio <adrian.antonio@marinebenefits.no>
2020-10-17 14:36:23 +0800

    Initial Commit

---
+++ ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by MBAS on 10/17/20.
+//  Copyright © 2020 Adrian Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
